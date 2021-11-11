555257243c175e2c4e9545c18edfa14e6b60681d
Owner <deema@danon.tzafonet.org.il>
2021-11-11 09:31:32 +0200

    Initial commit

---
+++ app/src/main/java/hajar/ashwah/ramzor/application/MainActivity.java
@@ -0,0 +1,14 @@
+package hajar.ashwah.ramzor.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+    }
+}
---
+++ app/src/main/java/hajar/ashwah/ramzor/application/SplashScreen.java
@@ -0,0 +1,33 @@
+package hajar.ashwah.ramzor.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        //Thread : 1
+        Thread th=new Thread(){
+            //Thread : 2
+            @Override
+            public void run() {
+                // المقطع الذي سيعمل بالتزامن مع مقاطع اخرى
+                //Thread : 3
+                int ms=3*1000;//milliseconds
+                try {
+                    sleep(ms);
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        // Thread : 4
+        th.start();
+    }
+}
